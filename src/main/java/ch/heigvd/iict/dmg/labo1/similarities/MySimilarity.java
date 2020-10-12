050aa16cf32aa98b33c80a4b8ef36181b52e34ee
Robin <r.demarta@outlook.com>
2020-10-12 18:49:19 +0200

    MySimilarity class

--- src/main/java/ch/heigvd/iict/dmg/labo1/similarities/MySimilarity.java
+++ src/main/java/ch/heigvd/iict/dmg/labo1/similarities/MySimilarity.java
@@ -6,4 +6,19 @@ public class MySimilarity extends ClassicSimilarity {
 
 	// TODO student
 	// Implement the functions described in section "Tuning the Lucene Score"
+
+    @Override
+    public float lengthNorm(int numTerms) {
+        return 1;
+    }
+
+    @Override
+    public float tf(float freq) {
+        return (float)(1 + Math.log(freq));
+    }
+
+    @Override
+    public float idf(long docFreq, long docCount) {
+        return (float)Math.log((double)docCount / (docFreq + 1)+1);
+    }
 }
